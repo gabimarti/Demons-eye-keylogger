80fd5b6345e95fcf4d1c97efd915ddcf9247b5e6
GM <44980795+foratnegre@users.noreply.github.com>
2019-07-28 10:27:51 +0200

    Update demonseye-monitor.py

--- demonseye-monitor.py
+++ demonseye-monitor.py
@@ -13,7 +13,7 @@
 # License:      GPL
 #-----------------------------------------------------------------------------------------------------------
 #
- 
+
 import socket
 import wx
 import wx.xrc
@@ -45,46 +45,46 @@ class MainWindow(wx.Frame):
 
 class MainFrame ( wx.Frame ):
     def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Demon's Eye Monitor", pos = wx.DefaultPosition, size = wx.Size( 651,490 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
-		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
-		bSizer2 = wx.BoxSizer( wx.VERTICAL )
-		self.m_toolBar1 = wx.ToolBar( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TB_HORIZONTAL )
-		self.m_toolBar1.Realize()
-		bSizer2.Add( self.m_toolBar1, 0, wx.EXPAND, 5 )
-		self.m_notebook1 = wx.Notebook( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer2.Add( self.m_notebook1, 1, wx.EXPAND |wx.ALL, 5 )
-		self.SetSizer( bSizer2 )
-		self.Layout()
-		self.m_menubar1 = wx.MenuBar( 0 )
-		self.m_menu_program = wx.Menu()
-		self.m_menuItem_exitprogram = wx.MenuItem( self.m_menu_program, wx.ID_ANY, u"Exit Program", wx.EmptyString, wx.ITEM_NORMAL )
-		self.m_menu_program.Append( self.m_menuItem_exitprogram )
+        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Demon's Eye Monitor", pos = wx.DefaultPosition, size = wx.Size( 651,490 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+        self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+        bSizer2 = wx.BoxSizer( wx.VERTICAL )
+        self.m_toolBar1 = wx.ToolBar( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TB_HORIZONTAL )
+        self.m_toolBar1.Realize()
+        bSizer2.Add( self.m_toolBar1, 0, wx.EXPAND, 5 )
+        self.m_notebook1 = wx.Notebook( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
+        bSizer2.Add( self.m_notebook1, 1, wx.EXPAND |wx.ALL, 5 )
+        self.SetSizer( bSizer2 )
+        self.Layout()
+        self.m_menubar1 = wx.MenuBar( 0 )
+        self.m_menu_program = wx.Menu()
+        self.m_menuItem_exitprogram = wx.MenuItem( self.m_menu_program, wx.ID_ANY, u"Exit Program", wx.EmptyString, wx.ITEM_NORMAL )
+        self.m_menu_program.Append( self.m_menuItem_exitprogram )
 
-		self.m_menubar1.Append( self.m_menu_program, u"Program" )
+        self.m_menubar1.Append( self.m_menu_program, u"Program" )
 
-		self.m_menu_monitor = wx.Menu()
-		self.m_menuItem_serverStart = wx.MenuItem( self.m_menu_monitor, wx.ID_ANY, u"Server Start", wx.EmptyString, wx.ITEM_NORMAL )
-		self.m_menu_monitor.Append( self.m_menuItem_serverStart )
+        self.m_menu_monitor = wx.Menu()
+        self.m_menuItem_serverStart = wx.MenuItem( self.m_menu_monitor, wx.ID_ANY, u"Server Start", wx.EmptyString, wx.ITEM_NORMAL )
+        self.m_menu_monitor.Append( self.m_menuItem_serverStart )
 
-		self.m_menuItem_serverStop = wx.MenuItem( self.m_menu_monitor, wx.ID_ANY, u"Server Stop", wx.EmptyString, wx.ITEM_NORMAL )
-		self.m_menu_monitor.Append( self.m_menuItem_serverStop )
+        self.m_menuItem_serverStop = wx.MenuItem( self.m_menu_monitor, wx.ID_ANY, u"Server Stop", wx.EmptyString, wx.ITEM_NORMAL )
+        self.m_menu_monitor.Append( self.m_menuItem_serverStop )
 
-		self.m_menubar1.Append( self.m_menu_monitor, u"Monitor" )
+        self.m_menubar1.Append( self.m_menu_monitor, u"Monitor" )
 
-		self.m_menu_help = wx.Menu()
-		self.m_menuItem_about = wx.MenuItem( self.m_menu_help, wx.ID_ANY, u"About", wx.EmptyString, wx.ITEM_NORMAL )
-		self.m_menu_help.Append( self.m_menuItem_about )
+        self.m_menu_help = wx.Menu()
+        self.m_menuItem_about = wx.MenuItem( self.m_menu_help, wx.ID_ANY, u"About", wx.EmptyString, wx.ITEM_NORMAL )
+        self.m_menu_help.Append( self.m_menuItem_about )
 
-		self.m_menubar1.Append( self.m_menu_help, u"Help" )
+        self.m_menubar1.Append( self.m_menu_help, u"Help" )
 
-		self.SetMenuBar( self.m_menubar1 )
+        self.SetMenuBar( self.m_menubar1 )
 
-		self.m_statusBar1 = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
+        self.m_statusBar1 = self.CreateStatusBar( 1, wx.STB_SIZEGRIP, wx.ID_ANY )
 
-		self.Centre( wx.BOTH )
+        self.Centre( wx.BOTH )
 
-	def __del__( self ):
-		pass
+    def __del__( self ):
+        pass
 
 
 
